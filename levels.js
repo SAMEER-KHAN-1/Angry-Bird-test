d2f53147bda981257ad9cbdcd0633a4da21a9cd4
SAMEER-KHAN-1 <sameerkhan654kk@gmail.com>
2026-09-25 23:37:51 +0530

    Fixed level 1 collapsing at start by spawning it already settled

--- levels.js
+++ levels.js
@@ -3,19 +3,18 @@ const LEVELS = [
         name: "Level 1",
         birds: 5,
         objects: [
-            { type: 'box', x: 700, y: 320, w: 70, h: 70 },
-            { type: 'box', x: 920, y: 320, w: 70, h: 70 },
-            { type: 'pig', x: 810, y: 350 },
-            { type: 'log', x: 810, y: 260, length: 300, angle: Math.PI / 2 },
+            { type: 'box', x: 700, y: 355, w: 70, h: 70 },
+            { type: 'box', x: 920, y: 355, w: 70, h: 70 },
+            { type: 'pig', x: 810, y: 365 },
+            { type: 'log', x: 810, y: 310, length: 300, angle: Math.PI / 2 },
 
-            { type: 'box', x: 700, y: 240, w: 70, h: 70 },
-            { type: 'box', x: 920, y: 240, w: 70, h: 70 },
-            { type: 'pig', x: 810, y: 220 },
-            { type: 'log', x: 810, y: 180, length: 300, angle: Math.PI / 2 },
+            { type: 'box', x: 700, y: 265, w: 70, h: 70 },
+            { type: 'box', x: 920, y: 265, w: 70, h: 70 },
+            { type: 'pig', x: 810, y: 275 },
+            { type: 'log', x: 810, y: 220, length: 300, angle: Math.PI / 2 },
 
-            { type: 'box', x: 810, y: 160, w: 70, h: 70 },
-            { type: 'log', x: 760, y: 120, length: 150, angle: Math.PI / 7 },
-            { type: 'log', x: 870, y: 120, length: 150, angle: -Math.PI / 7 }
+            { type: 'box', x: 810, y: 175, w: 70, h: 70 },
+            { type: 'log', x: 810, y: 130, length: 200, angle: Math.PI / 2 }
         ]
     },
     {
